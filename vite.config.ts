489c6b7eab69f433af6b43b74507210bfb4102d4
Ben <isieldor@gmail.com>
2022-08-16 13:38:12 -0500

    base drop

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "/infinityHorse/",
   build: {
     outDir: "../docs",
-    emptyOutDir: true
+    emptyOutDir: true,
   }
 })
